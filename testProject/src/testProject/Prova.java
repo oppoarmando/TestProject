81b9f71394b09b8445469852861afe516d5f06da
armando.maria.oppo <armando.maria.oppo@NAPGLA7IJEM4KMW.dir.svc.accenture.com>
2022-04-13 11:56:14 +0200

    modifiche più aggiunta

---
+++ testProject/src/testProject/Prova.java
@@ -0,0 +1,8 @@
+package testProject;
+
+public class Prova {
+	
+	public void prova() {
+		System.out.println("Proviamo un metodo");
+	}
+}
--- testProject/src/testProject/Test.java
+++ testProject/src/testProject/Test.java
@@ -2,6 +2,8 @@ package testProject;
 
 public class Test {
 	public static void main(String args[]) {
-		System.out.println("Ciao");
+		Prova oggiprova = new Prova();
+		System.out.println("Test");
+		oggiprova.prova();
 	}
 }
