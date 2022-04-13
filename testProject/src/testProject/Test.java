4486f4dd20fcd3aefcba55c736872900577e41c0
armando.maria.oppo <armando.maria.oppo@NAPGLA7IJEM4KMW.dir.svc.accenture.com>
2022-04-13 12:27:00 +0200

    leggero cambio

--- testProject/src/testProject/Test.java
+++ testProject/src/testProject/Test.java
@@ -3,7 +3,7 @@ package testProject;
 public class Test {
 	public static void main(String args[]) {
 		Prova oggiprova = new Prova();
-		Aggiungo oggagg = new Aggiungo();
+		Aggiungo oggAgg = new Aggiungo();
 		System.out.println("Test");
 		oggiprova.prova();
 	}
